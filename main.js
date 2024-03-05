5e6ea0e5447f15d7dbcc3dec241d1706866fd961
Gajendra <raogajendra27@gmail.com>
2024-03-05 12:53:48 +0530

    added the js code, working on displaying the error if the feilds are empty

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-let submitButton = document.getElementById('submitbtn').addEventListener('click', mybtn)
+let submitButton = document.getElementById('submitbtn')
+submitButton.addEventListener('click', mybtn)
 
 let firstName;
 let lastName;
@@ -6,16 +7,27 @@ let email;
 let password;
 
 function mybtn(){
-
+    
     firstName = document.getElementById('firsttxt').value
     lastName = document.getElementById('lasttxt').value
     email = document.getElementById('emailtxt').value
     password = document.getElementById('passwordtxt').value
+
     
-  if(firstName.value === "" || lastName.value ===""|| email.value ==="" || password.value ===""){
+    document.getElementById('first-name-display').textContent ="";
+    document.getElementById('last-name-display').textContent ="";
+    document.getElementById('email-display').textContent ="";
+    document.getElementById('password-display').textContent ="";
+    
+    if(firstName === "" || lastName ===""|| email === "" || password ===""){
+
     document.getElementById('first-name-display').textContent ="First Name cannot be empty";
-    document.getElementById('last-name-display').textContent ="First Name cannot be empty";
-    document.getElementById('first-name-display').textContent ="First Name cannot be empty";
-    document.getElementById('first-name-display').textContent ="First Name cannot be empty";
+    document.getElementById('last-name-display').textContent ="Last Name cannot be empty";
+    document.getElementById('email-display').textContent ="Email Address cannot be empty";
+    document.getElementById('password-display').textContent ="Password cannot be empty";
+
+    
+}else{
+    console.log('error');
   }
 }
