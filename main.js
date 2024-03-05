72b2483c51c86cb2c07d9f4ae252deed51bc4e94
Gajendra <raogajendra27@gmail.com>
2024-03-05 13:31:58 +0530

    added alert when the form is  submitted, V.1 Done, working on V.2

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ let email;
 let password;
 
 function mybtn(){
-    event.preventDefault();
-    
+    event.preventDefault(); // By adding event.preventDefault();, you prevent the form from being submitted, and the form will be not cleared
+
     firstName = document.getElementById('firsttxt').value
     lastName = document.getElementById('lasttxt').value
     email = document.getElementById('emailtxt').value
@@ -27,8 +27,16 @@ function mybtn(){
     document.getElementById('email-display').textContent ="Email Address cannot be empty";
     document.getElementById('password-display').textContent ="Password cannot be empty";
 
+
     
 }else{
-    console.log('error');
+    firstName = document.getElementById('firsttxt').value = "";
+    lastName = document.getElementById('lasttxt').value = "";
+    email = document.getElementById('emailtxt').value = "";
+    password = document.getElementById('passwordtxt').value = "";
+
+     alert('The form was Submitted Successfully')
+
   }
 }
+
